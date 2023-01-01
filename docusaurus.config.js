c88fe5b4f600b63430a989428dde803fc95a2433
rohit <rohitnandi12@gmail.com>
2023-01-01 19:50:37 +0530

    Changed name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Rohit Nandi',
   tagline: 'Dinosaurs are cool',
   url: 'https://rohitnandi12.github.io',
   baseUrl: '/testDocu/',
