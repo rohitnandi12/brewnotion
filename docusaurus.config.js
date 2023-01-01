5d65f28febcc21e3135354699d239b093cf1fb6d
rohit <rohitnandi12@gmail.com>
2023-01-01 19:13:09 +0530

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://rohitnandi12.github.io',
-  baseUrl: '/',
+  baseUrl: '/testDocu/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
